f61dad73044dd9467106bcbea808020dd197450f
kulekule2003 <okureenock@gmail.com>
2025-07-10 12:47:43 +0300

    added firebase

--- resqnet/resqnet/lib/main.dart
+++ resqnet/resqnet/lib/main.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 // import 'screens/SignUp_Screen.dart';
 import 'screens/Login_Screen.dart';
-// import 'package:firebase_core/firebase_core.dart';
-// import 'firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
-  //WidgetsFlutterBinding.ensureInitialized();
-  //await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+Future <void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   runApp(MyApp());
 }
