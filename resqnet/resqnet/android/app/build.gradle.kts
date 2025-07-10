9b4e78f807ec4a6fc6feadc97bb461522f1b47e3
kulekule2003 <okureenock@gmail.com>
2025-07-11 00:09:24 +0300

    added firebase

--- resqnet/resqnet/android/app/build.gradle.kts
+++ resqnet/resqnet/android/app/build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 android {
     namespace = "com.example.resqnet"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "27.0.12077973"
+    ndkVersion = "29.0.13113456"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
--- resqnet/resqnet/android/build.gradle.kts
+++ resqnet/resqnet/android/build.gradle.kts
@@ -1,13 +1,24 @@
-allprojects {
+buildscript {
     repositories {
         google()
         mavenCentral()
     }
     dependencies {
-        classpath("com.google.gms:google-services:4.4.1")
+        classpath("com.android.tools.build:gradle:8.0.2") // example version, adjust as needed
+        classpath("com.google.gms:google-services:4.4.1") // Google services plugin
+        // Add Kotlin Gradle plugin if needed:
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.0")
     }
 }
 
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+// Your existing code for build directories and clean task below
 val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
 rootProject.layout.buildDirectory.value(newBuildDir)
 
@@ -15,6 +26,7 @@ subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
+
 subprojects {
     project.evaluationDependsOn(":app")
 }
@@ -22,4 +34,3 @@ subprojects {
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
-
